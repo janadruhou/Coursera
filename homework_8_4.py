d56baf851b48e902d18f69f8bda656c7acd6a664
Ondrej <hrabina.ondra@gmail.com>
2018-11-06 19:01:42 +0100

    3rd attempt

--- homework_8_4.py
+++ homework_8_4.py
@@ -4,10 +4,10 @@ for line in romeo:
     lines = line.strip()
     s_lines = lines.split()
     
-    for word in s_lines:
-        if word is not in final:
-            final.append(word)
-        else: 
-            continue
+    
+    if word is not in final:
+        final.append(word)
+    else: 
+        continue
 
-    print((final)gi
+    print(final)
