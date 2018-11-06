4a8ccfd1d5ca668ac9c0595fd608bd8cb58ba5bc
Ondrej <hrabina.ondra@gmail.com>
2018-11-06 18:56:55 +0100

    2nd attempt

--- homework_8_4.py
+++ homework_8_4.py
@@ -1,3 +1,13 @@
 romeo = open('romeo.txt')
+final = list()
+for line in romeo:
+    lines = line.strip()
+    s_lines = lines.split()
+    
+    for word in s_lines:
+        if word is not in final:
+            final.append(word)
+        else: 
+            continue
 
-print(romeo)
+    print((final)
