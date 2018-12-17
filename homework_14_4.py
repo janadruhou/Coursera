5c47766dedc70fa9a9610b266201786d4afd06a5
Ondrej <hrabina.ondra@gmail.com>
2018-12-17 11:26:25 +0100

    2nd attempt

--- homework_14_4.py
+++ homework_14_4.py
@@ -1,51 +1,28 @@
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
+import http
+import sqlite3
+import time
 
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
+source = "http://py4e-data.dr-chuck.net/json?"
 
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
-else :
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
-
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+test_institution = "South Federal University"
+my_institution = "Indiana University at Bloomington"
 
 while True:
-    address = input('Enter location: ')
+    address = input('Enter institution: ')
     if len(address) < 1: break
+    
+    parms = {"address": address}
+    
+    url = source + urllib.parse.urlencode(parms)
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
+    print ('Digging', url)
+    data = urllib.request.urlopen(url).read().decode()
+    print("Fetched", len(data), 'characters')
 
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    print('Retrieved', len(data), 'characters')
+    fetched = json.loads(data)
 
-    try:
-        js = json.loads(data)
-    except:
-        js = None
-
-    if not js or 'status' not in js or js['status'] != 'OK':
-        print('==== Failure To Retrieve ====')
-        print(data)
-        continue
-
-    print(json.dumps(js, indent=4))
-
-    lat = js['results'][0]['geometry']['location']['lat']
-    lng = js['results'][0]['geometry']['location']['lng']
-    print('lat', lat, 'lng', lng)
-    location = js['results'][0]['formatted_address']
-    print(location)
+    print(fetched)
+    
